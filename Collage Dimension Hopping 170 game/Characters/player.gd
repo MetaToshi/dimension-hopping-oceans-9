a164b41c1ea074c6de7bff730daab9fe8049c1d9
ethanurl <130177026+ethanurl@users.noreply.github.com>
2023-10-25 19:06:44 -0700

    Created base version
    This version is missing attacks and destructible enemies.
    Those should be added by the end of the day :3

---
+++ Collage Dimension Hopping 170 game/Characters/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var speed : float = 500.0
+@export var jump_velocity : float = -1000
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	#Make character move
+	velocity.x = speed
+	if velocity.x <= speed:
+		pass
+	
+	move_and_slide()
+	
+	#Working on making the chracter destroy stuff
+func attack():
+	if Input.is_action_just_pressed("attack"):
+		pass
+func _on_attacking_area_entered(area):
+	if area.is_in_group("breakable"):
+		area.destroy()
