37a19272b2248e68aa784f242ced22964e1dca61
MetaToshi <MetaToshi@users.noreply.github.com>
2023-11-02 10:36:11 -0700

    Added Audio Assets
    Background muisic changes with each dimension.
    Bug: trying to figure out how to change attack audio and visual for each dimension

--- Collage Dimension Hopping 170 game/Characters/player.gd
+++ Collage Dimension Hopping 170 game/Characters/player.gd
@@ -15,7 +15,10 @@ func stop_anim() -> void:
 	anim.stop()
 """
 
-
+func _ready():
+	$DIM1PLAYER.play("run")
+	$DIM1BGMUSIC.play()
+	
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -37,8 +40,10 @@ func _physics_process(delta):
 func _process(_delta):
 	if Input.is_action_just_pressed("attack"):
 		$Chara.play("attack")
+		$FireSpell.play()
 		
 func respawn():
+	$DIM1BGMUSIC.play()
 	position.x = 0
 	position.y = 0
 	pass
@@ -46,29 +51,31 @@ func respawn():
 # Player entering through portal
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("Portal"):
-		$PlayerTEST.texture=ResourceLoader.load("res://TestAssets/testFloorTile.png")
 		$PLAYERDIM1.visible = false
 		$PLAYERDIM2.visible = true
 		$PLAYERDIM3.visible = false
 		$ParallaxBackgroundDIM1.visible = false
 		$ParallaxBackgroundDIM2.visible = true
+		$DIM1BGMUSIC.stop()
+		$DIM2BGMUSIC.play()
 		position.x = 0
 		position.y = 1500
 		speed = 750
 		jump_velocity = -1500
 		
 	if area.is_in_group("portal2"):
-		$PLAYERDIM2.texture=ResourceLoader.load("res://TestAssets/testFloorTile.png")
 		$PLAYERDIM1.visible = false
 		$PLAYERDIM2.visible = false
 		$PLAYERDIM3.visible = true
 		$ParallaxBackgroundDIM1.visible = false
 		$ParallaxBackgroundDIM2.visible = false
+		$DIM2BGMUSIC.stop()
+		$DIM3BGMUSIC.play()
 		# $ParallaxLayer1.parallax_scale = Vector2() # changes size
 		position.x = 0
 		position.y = 3000
 		speed = 400
-		jump_velocity = -1800
+		jump_velocity = -1200
 		
 	if area.is_in_group("Portal3"):
 		for i in $"..".get_children():
