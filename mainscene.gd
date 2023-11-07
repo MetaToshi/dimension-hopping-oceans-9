b4ab4da10280d2e411342092929ed3e49d1a8ab3
Seren <122656355+serengirlaxy@users.noreply.github.com>
2023-11-06 18:47:49 -0800

    HEwwo??

---
+++ mainscene.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+@onready var breakables = get_tree().get_nodes_in_group("breakableparent")
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		$PauseMenu.visible = true
+		$PauseMenu.position = $Player.position
+		$PauseMenu.position.y -= 225
+		$PauseMenu.position.x -= 50
+		get_tree().paused = true
+	if $Player.position.x <= 100:
+		print($Player.position)
+		for i in breakables:
+			if i.position.y >= 9000:
+				print(i.position)
+				i.position.y -= 10000
+				print(i.position)
+	if Input.is_action_just_pressed("Restart from Checkpoint"):
+		for i in breakables:
+			if i.position.y >= 9000:
+				print(i.position)
+				i.position.y -= 10000
+				print(i.position)
+		pass
+		
+		
+func _on_back_to_game_pressed():
+	get_tree().paused = false
+	$PauseMenu.visible = false
+
+
+func _on_back_to_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scripts/main_menu.tscn")
+
+
+func _on_controls_pressed():
+	$ControlMenuX.position = $PauseMenu.position
+	$ControlMenu.position = $PauseMenu.position
+	
+	$ControlMenu.position.y -= 90
+	$ControlMenu.position.x += 310
+	
+	$ControlMenuX.position.y -= 190
+	$ControlMenuX.position.x += 655
+	
+	$ControlMenuX.visible = true
+	$ControlMenu.visible = true
+	
+
+
+func _on_control_menu_x_pressed():
+	$ControlMenuX.visible = false
+	$ControlMenu.visible = false
+
+
+func _on_reload_checkpoint_pressed():
+	get_tree().paused = false
+	$PauseMenu.visible = false
+	$Player.respawn()
+	for i in breakables:
+		if i.position.y >= 10000:
+			i.position.y -= 10000
+
