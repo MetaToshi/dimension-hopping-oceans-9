161e81322af99a970b0bd66fb7d3bdc5457afe9c
MetaToshi <MetaToshi@users.noreply.github.com>
2023-11-07 10:35:44 -0800

    Nope! :D Optimized Dim2
    modified some stuff, dim 2 was laggy, so i changed around some things.

--- Collage Dimension Hopping 170 game/CREDITS.gd
+++ Collage Dimension Hopping 170 game/CREDITS.gd
@@ -26,4 +26,4 @@ func _ready():
 #		currentSprite = 1
 	
 func _on_continue_button_pressed():
-	get_tree().change_scene_to_file("res://mainmenu.tscn")
+	get_tree().change_scene_to_file("res://Scripts/mainmenu.tscn")
